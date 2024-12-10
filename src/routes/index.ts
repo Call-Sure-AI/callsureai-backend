ff3ed23277e9d052a1043c119b9581a5f4b73e4a
M-A-N-I-K <manikdhingra0582@gmail.com>
2024-12-10 07:22:59 +0530

    added additional route to check server status

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,5 +4,6 @@ import authRoutes from './auth.routes'
 const router = Router();
 
 router.use('/auth', authRoutes)
+router.use('/ping', (req, res) => res.status(200).json({ message: 'pong' }))
 
 export { router };
