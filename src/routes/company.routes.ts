62d68395015ac00e22d802aa2dc5aa4f622f101a
M-A-N-I-K <manikdhingra0582@gmail.com>
2024-12-15 21:23:54 +0530

    added auth middleware:

--- src/routes/company.routes.ts
+++ src/routes/company.routes.ts
@@ -2,20 +2,18 @@ import { Router } from 'express';
 import { CompanyController } from '../controllers/company.controller';
 import { createCompanySchema, updateCompanySchema } from '../middleware/validators/company-validator';
 import { validateRequest } from '../middleware/validate';
+import { authMiddleware } from '../middleware/auth-middleware';
 
 
 const router = Router();
 
-// TODO : Implement Auth Middleware
-// router.use(authenticateUser);
-
-router.get('', CompanyController.getAllForUser);
-router.get('/:id', CompanyController.getById);
-router.get('/user/:user_id', CompanyController.getByUserId);
-router.post('', validateRequest(createCompanySchema), CompanyController.create);
-router.post('/create-or-update', validateRequest(createCompanySchema), CompanyController.createOrUpdate);
-router.put('/:id', validateRequest(updateCompanySchema), CompanyController.update);
-router.delete('/:id', CompanyController.delete);
-router.post('/:id/regenerate-api-key', CompanyController.regenerateApiKey);
+router.get('', authMiddleware, CompanyController.getAllForUser);
+router.get('/:id', authMiddleware, CompanyController.getById);
+router.get('/user/:user_id', authMiddleware, CompanyController.getByUserId);
+router.post('', authMiddleware, validateRequest(createCompanySchema), CompanyController.create);
+router.post('/create-or-update', authMiddleware, validateRequest(createCompanySchema), CompanyController.createOrUpdate);
+router.put('/:id', authMiddleware, validateRequest(updateCompanySchema), CompanyController.update);
+router.delete('/:id', authMiddleware, CompanyController.delete);
+router.post('/:id/regenerate-api-key', authMiddleware, CompanyController.regenerateApiKey);
 
 export default router;
