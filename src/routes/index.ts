e678ecc2a060150715a8b90fec31e7e18b8125ac
M-A-N-I-K <manikdhingra0582@gmail.com>
2024-12-22 23:09:16 +0530

    added health check

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,5 +9,6 @@ router.use('/auth', authRoutes);
 router.use('/company', companyRoutes);
 router.use('/agent', agentRoutes);
 router.use('/check-status', (req, res) => res.status(200).json({ message: 'active' }))
+router.use('/ping', (req, res) => res.status(500).json({ message: 'pong' }))
 
 export { router };
